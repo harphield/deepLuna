117677e1e00e622527fd2699a994b9cf16b7b9fb
harphield <harphield@gmail.com>
2021-09-23 09:01:10 +0200

    Extracting and rebuilding sysmes_text.dat

---
+++ extract_sysmes.py
@@ -0,0 +1,46 @@
+sysmes = open('SYSMES_TEXT.DAT', 'rb')
+
+data = sysmes.read()
+
+count_offset = '0x4'
+
+int_pos = int(count_offset, 16)
+string_count = int.from_bytes(data[int_pos:int_pos + 4], byteorder='little')
+
+print(string_count)
+
+header_offset = '0x18'
+
+# first we read the header with the string offsets
+positions = []
+first_position = int(header_offset, 16)
+i = first_position
+while i < first_position + string_count * 8:
+    positions.append(int.from_bytes(data[i:i + 8], byteorder='little'))
+    i += 8
+
+# then we read the strings on those positions
+
+output = open('sysmes_text.txt', 'w+', encoding="utf-8")
+
+for position in positions:
+    int_pos = position
+
+    values = []
+    texts = []
+
+    value = data[int_pos]
+
+    while value != 0:
+        values.append(value)
+        int_pos += 1
+        value = data[int_pos]
+
+    text = bytearray(values).decode('utf-8')
+    texts.append(text)
+    print(hex(position) + ': ' + text)
+    output.write(text + "\n")
+
+
+sysmes.close()
+output.close()
---
+++ rebuild_sysmes.py
@@ -0,0 +1,17 @@
+# the original sysmes here
+sysmes = open('SYSMES_TEXT.DAT', 'rb')
+
+# the translated texts here
+translation = open('sysmes_text.txt', 'rb')
+
+# output of new sysmes here
+new_sysmes = open('SYSMES_TEXT_NEW.DAT', 'wb')
+
+# TODO prepare header and footer
+# TODO write header
+# TODO write strings
+# TODO write footer
+
+sysmes.close()
+translation.close()
+new_sysmes.close()
